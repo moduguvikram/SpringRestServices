a156896c822705692c3b4ee7034deb298ee53a3a
Vikram Modugu <moduguvikram@gmail.com>
2017-08-09 22:17:15 -0500

    Add files via upload

---
+++ src/main/java/com/ws/spring/rest/controller/DataAccessLayer.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.ws.spring.rest.controller;
+
+/**
+ * @author modug
+ *
+ */
+public class DataAccessLayer {
+
+	/**
+	 * 
+	 */
+	public DataAccessLayer() {
+		
+	}
+	
+	public Movie getMovie(int id)
+	{
+		
+		return new Movie();
+	}
+	
+	public Movie getMovie(String name)
+	{
+		
+		return new Movie();
+	}
+
+}
